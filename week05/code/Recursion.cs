5b0e13ccec2fcd849901387b2ffb757bb96c0357
Edwino2400 <edwinchigara@gmail.com>
2026-02-10 13:30:39 +0200

    Week 5  assignment

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -2,20 +2,13 @@ using System.Collections;
 
 public static class Recursion
 {
-    /// <summary>
-    /// #############
-    /// # Problem 1 #
-    /// #############
-    /// Using recursion, find the sum of 1^2 + 2^2 + 3^2 + ... + n^2
-    /// and return it.  Remember to both express the solution 
-    /// in terms of recursive call on a smaller problem and 
-    /// to identify a base case (terminating case).  If the value of
-    /// n <= 0, just return 0.   A loop should not be used.
-    /// </summary>
+    /// Problem 1 Solution
     public static int SumSquaresRecursive(int n)
     {
-        // TODO Start Problem 1
-        return 0;
+        
+         if (n <= 0)
+            return 0;
+
     }
 
     /// <summary>
@@ -38,9 +31,40 @@ public static class Recursion
     /// and the length of the letters list).
     /// </summary>
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
+    ///PROBLEM 2 SOLUTION
+{
+    List<string> results = new List<string>();
+    GeneratePermutations(letters, size, "", new bool[letters.Length], results);
+    return results;
+}
+
+private static void GeneratePermutations(
+    char[] letters,
+    int size,
+    string current,
+    bool[] used,
+    List<string> results)
+{
+    // when we reach the desired size, store result
+    if (current.Length == size)
     {
-        // TODO Start Problem 2
+        results.Add(current);
+        return;
     }
+
+    // try adding each unused letter
+    for (int i = 0; i < letters.Length; i++)
+    {
+        if (!used[i])
+        {
+            used[i] = true;
+            GeneratePermutations(letters, size, current + letters[i], used, results);
+            used[i] = false;
+        }
+    }
+}
+
+    
 
     /// <summary>
     /// #############
@@ -85,23 +109,35 @@ public static class Recursion
     /// the function for you to complete this task.
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
-    {
-        // Base Cases
-        if (s == 0)
-            return 0;
-        if (s == 1)
-            return 1;
-        if (s == 2)
-            return 2;
-        if (s == 3)
-            return 4;
+{
+    // If memo dictionary is null, create it (recursion-compatible)
+    if (remember == null)
+        remember = new Dictionary<int, decimal>();
 
-        // TODO Start Problem 3
+    // Base cases required by assignment
+    if (s == 0)
+        return 0;
+    if (s == 1)
+        return 1;
+    if (s == 2)
+        return 2;
+    if (s == 3)
+        return 4;
 
-        // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
-        return ways;
-    }
+    // PROBLEM 3 SOLUTION
+    if (remember.ContainsKey(s))
+        return remember[s];
+
+    //
+    decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+
+    // the result
+    remember[s] = ways;
+
+    //recursive result
+    return ways;
+}
+
 
     /// <summary>
     /// #############
@@ -116,28 +152,66 @@ public static class Recursion
     /// Using recursion, insert all possible binary strings for a given pattern into the results list.  You might find 
     /// some of the string functions like IndexOf and [..X] / [X..] to be useful in solving this problem.
     /// </summary>
-    public static void WildcardBinary(string pattern, List<string> results)
+   public static void WildcardBinary(string pattern, List<string> results)
+{
+    //Problem 4 solution
+    int index = pattern.IndexOf('*');
+
+    // Base case: no wildcards left
+    if (index == -1)
     {
-        // TODO Start Problem 4
+        results.Add(pattern);
+        return;
     }
+
+    // Replace * with 0
+    string withZero = pattern[..index] + "0" + pattern[(index + 1)..];
+    WildcardBinary(withZero, results);
+
+    // Replace * with 1
+    string withOne = pattern[..index] + "1" + pattern[(index + 1)..];
+    WildcardBinary(withOne, results);
+}
 
     /// <summary>
     /// Use recursion to insert all paths that start at (0,0) and end at the
     /// 'end' square into the results list.
     /// </summary>
-    public static void SolveMaze(List<string> results, Maze maze, int x = 0, int y = 0, List<ValueTuple<int, int>>? currPath = null)
+   public static void SolveMaze(List<string> results, Maze maze,
+                             int x = 0, int y = 0,
+                             List<ValueTuple<int, int>>? currPath = null)
+{
+    // Initialize currPath on first call
+    if (currPath == null)
+        currPath = new List<ValueTuple<int, int>>();
+
+    //Solution for Problem 5
+
+    // Add current position to path
+    currPath.Add((x, y));
+
+    // If we reached end, save path
+    if (maze.IsEnd(x, y))
     {
-        // If this is the first time running the function, then we need
-        // to initialize the currPath list.
-        if (currPath == null) {
-            currPath = new List<ValueTuple<int, int>>();
-        }
-        
-        // currPath.Add((1,2)); // Use this syntax to add to the current path
-
-        // TODO Start Problem 5
-        // ADD CODE HERE
-
-        // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+        results.Add(currPath.AsString());
     }
+    else
+    {
+        // Try all directions
+        if (maze.IsValidMove(x + 1, y, currPath))
+            SolveMaze(results, maze, x + 1, y, currPath);
+
+        if (maze.IsValidMove(x - 1, y, currPath))
+            SolveMaze(results, maze, x - 1, y, currPath);
+
+        if (maze.IsValidMove(x, y + 1, currPath))
+            SolveMaze(results, maze, x, y + 1, currPath);
+
+        if (maze.IsValidMove(x, y - 1, currPath))
+            SolveMaze(results, maze, x, y - 1, currPath);
+    }
+
+    // tracking back
+    currPath.RemoveAt(currPath.Count - 1);
+}
 }
